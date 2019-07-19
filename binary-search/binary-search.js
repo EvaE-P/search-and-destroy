14bffbedfa1a5fd94fc51c6284e312d9317996c2
Morgan Mattone <mpmkid@gmail.com>
2019-07-19 11:54:43 -0400

    finished binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,9 +1,36 @@
-'use strict';
+/* eslint-disable complexity */
+"use strict";
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
+const binarySearch = (array, target, head = 0, tail = array.length - 1) => {
+  // head = 0;
+  // tail = array.length - 1;
+  let midpoint = Math.floor((tail + head) / 2);
+  console.log("this is our midpoint", midpoint);
+  if (target > array[tail] || target < array[head]) {
+    return false;
+  }
+  if (
+    array[head] === target ||
+    array[tail] === target ||
+    array[midpoint] === target
+  ) {
+    return true;
+  } else if (target > array[midpoint]) {
+    console.log("this is our head", head);
+    head = midpoint + 1;
+    console.log("after assign", head);
+    return binarySearch(array, target, head, tail);
+  } else if (target < array[midpoint]) {
+    tail = midpoint - 1;
+    return binarySearch(array, target, head, tail);
+  } else if (head === tail) {
+    return false;
+  } else {
+    return false;
+  }
 };
+console.log(binarySearch([-3, -2, 0, 9, 10, 11, 12, 15, 17, 18, 19, 20], 25));
 
 /*
 	EXTRA CREDIT:
@@ -14,4 +41,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
