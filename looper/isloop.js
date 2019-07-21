63e303ed21b19e1685c1faf0e4ee54e7e50fe89b
Eva E-P <eva.emendoerfer@gmail.com>
2019-07-21 12:42:13 -0400

    isloop completed

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,27 @@
-'use strict';
+/* eslint-disable complexity */
+"use strict";
+const LinkedList = require("./linkedlist");
 
 //Complete this algo
-const isLoop = (linkedlist) => {
+const isLoop = linkedlist => {
+  let p1 = linkedlist.head.next.next;
+  let p2 = linkedlist.head.next;
+  if (linkedlist.head.next == null) {
+    return false;
+  }
+  while (p1 || p1.next || p2 || p2.next) {
+    if (p1 === p2) {
+      return true;
+    } else if (p1 && p1.next) {
+      p1 = p1.next.next;
+      p2 = p2.next;
+    } else {
+      return false;
+    }
+  }
 
+  return false;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +31,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
